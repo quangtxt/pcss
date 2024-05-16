fcb92286d0183e0a2f966f6d4dccd90af2bab45a
QuangNV <quangtxt2k2@gmail.com>
2024-05-16 22:53:48 +0700

    update config

--- Code/FE/src/pages/NotFoundPage/NotFoundPage.js
+++ Code/FE/src/pages/NotFoundPage/NotFoundPage.js
@@ -1,16 +1,16 @@
-import React, { Fragment } from 'react'
-import { Helmet } from 'react-helmet/es/Helmet'
-import { Wrapper } from './NotFoundPageStyled'
-import { Button, Result } from 'antd'
-import { useHistory } from 'react-router-dom'
+import React, { Fragment } from "react";
+import { Helmet } from "react-helmet/es/Helmet";
+import { Wrapper } from "./NotFoundPageStyled";
+import { Button, Result } from "antd";
+import { useHistory } from "react-router-dom";
 
-const NotFoundPage = props => {
-  const history = useHistory()
+const NotFoundPage = (props) => {
+  const history = useHistory();
 
   return (
     <Wrapper>
       <Helmet>
-        <title>404 | CMB Portal</title>
+        <title>404 | Pcms Portal</title>
       </Helmet>
       <Result
         status="404"
@@ -21,14 +21,14 @@ const NotFoundPage = props => {
             <Button onClick={() => history.goBack()}>
               Quay lại trang trước
             </Button>
-            <Button type={'primary'} onClick={() => history.push('/')}>
+            <Button type={"primary"} onClick={() => history.push("/")}>
               Về trang chủ
             </Button>
           </Fragment>
         }
       />
     </Wrapper>
-  )
-}
+  );
+};
 
-export default NotFoundPage
+export default NotFoundPage;
