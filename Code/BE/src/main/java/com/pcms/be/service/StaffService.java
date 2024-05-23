575e345115924d5b9e08a4a5b6e2fd004602f1e4
HQuang <boycghd989@gmail.com>
2024-05-23 18:20:25 +0700

    Update function check format excel

--- Code/BE/src/main/java/com/pcms/be/service/StaffService.java
+++ Code/BE/src/main/java/com/pcms/be/service/StaffService.java
@@ -3,11 +3,14 @@ package com.pcms.be.service;
 import com.pcms.be.domain.user.Member;
 import com.pcms.be.domain.user.User;
 import com.pcms.be.errors.Response;
+import com.pcms.be.errors.ServiceException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.Map;
 
 public interface StaffService {
+    public ResponseEntity<List<Integer>> checkFormatExcel(MultipartFile file) throws ServiceException;
     public ResponseEntity<String> addUserByExcel(MultipartFile file);
 }
